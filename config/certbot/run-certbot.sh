7ae5b87069d27a7fbfae4978a578085a4f910452
Cem Gencer <o.cem.gencer@gmail.com>
2025-06-28 10:29:15 +0300

    still phpfpm doesnt work as intended

--- config/certbot/run-certbot.sh
+++ config/certbot/run-certbot.sh
@@ -2,8 +2,8 @@
 
 letsencrypt certonly --webroot -w /var/www/letsencrypt -d "$CN" --agree-tos --email "$EMAIL" --non-interactive --text
 
-cp /etc/letsencrypt/archive/"$CN"/cert1.pem /var/certs/"$CN"-cert1.pem
-cp /etc/letsencrypt/archive/"$CN"/chain1.pem /var/certs/chain1.pem
-cp /etc/letsencrypt/archive/"$CN"/fullchain1.pem /var/certs/fullchain1.pem
-cp /etc/letsencrypt/archive/"$CN"/privkey1.pem /var/certs/"$CN"-privkey1.pem
+cp /etc/letsencrypt/archive/"$CN"/cert1.pem /var/certs/"$CN"-cert1.pem 2>/dev/null
+cp /etc/letsencrypt/archive/"$CN"/chain1.pem /var/certs/chain1.pem 2>/dev/null
+cp /etc/letsencrypt/archive/"$CN"/fullchain1.pem /var/certs/fullchain1.pem 2>/dev/null
+cp /etc/letsencrypt/archive/"$CN"/privkey1.pem /var/certs/"$CN"-privkey1.pem 2>/dev/null
 
